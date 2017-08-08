b80a3af100525fa466684a4d61751285a43a8c21
Mike Giordano <mgiord1987@gmail.com>
2017-08-08 19:59:28 -0400

    Get pretty text from description with bs4

--- description_bot.py
+++ description_bot.py
@@ -14,36 +14,17 @@ print("logged in")
 
 r = requests.get(input('type it > '))
 
-
-def find_raw_description(rawtext):
-    dopen = rawtext.find('>', rawtext.find('eow-description'))
-    dclose = rawtext.find('</p>', dopen)
-    return(rawtext[dopen + 1:dclose])
-
-
-def find_links(rawtext):
+def get_description(rawtext):
     page = BeautifulSoup(rawtext, "html.parser")
-    desc = page.find(id="eow-description")
-    print(desc)
-
-# def find_links(rawtext):
-#     for link in BeautifulSoup(rawtext,
-#                               "html.parser",
-#                               parse_only=SoupStrainer('a')):
-#         if link.has_attr('href'):
-#             url = link['href']
-#             linktext = link.string
-#             redlink = '[' + linktext + ']' + '(' + url + ')'
-#             link.unwrap()
-
-def prettify_text(rawtext):
-    breaks = find_links(rawtext)
-    breaks = rawtext.split('<br />')
-    for line in breaks:
-        print(line)
+    description = page.find(id="eow-description")
+    for br in description.find_all("br"):
+        br.replace_with("\n")
+    return(description.get_text())
 
 
-rawtext = find_raw_description(r.text)
-prettify_text(rawtext)
+
+# rawtext = find_raw_description(r.text)
+# prettify_text(rawtext)
+get_description(r.text)
 
 # print(rawtext)
